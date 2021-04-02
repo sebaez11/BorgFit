4aa092cd8bd948ff7686e7f653fec588d91bd656
Sebastian Baez <sebaez200011@gmail.com>
2021-04-02 10:57:58 -0500

    setted eureka server up

---
+++ backend/eureka-server-service/src/main/java/com/eureka/server/EurekaServerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerServiceApplication.class, args);
+	}
+
+}
